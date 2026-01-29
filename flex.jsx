3db5d17d3135676f4be08abe0471cde54807fa5e
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-01-30 03:10:00 +0530

    Create flex.jsx with a simple layout

---
+++ flex.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <div className='flex justify-between'>
+    <div>hi</div>
+    <div>hello</div>
+    <div>yobachho</div>
+
+  </div>
+
+}
+
+  
+
+export default App
