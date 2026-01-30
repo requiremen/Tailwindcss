c99b961b135dac560b974917fc1bf2b50d460361
Akshat Rastogi <154761569+requiremen@users.noreply.github.com>
2026-01-30 16:35:19 +0530

    Add App component with Revenuecard

---
+++ appforrevenuecomponent.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return <div className="grid grid-cols-4">
+    <Revenuecard title={"Amount pending"} amount={"92,000"} orderCount={13}/>
+    
+
+  </div>
+
+}
+
+  
+
+export default App
